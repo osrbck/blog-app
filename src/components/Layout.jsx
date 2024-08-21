5a8fa2bad8cdad13fb8c3e5598245927e1572bc7
Osman <osrbck@gmail.com>
2024-08-21 14:14:38 +0300

    Footer and ErrorPage

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = () => {
   return (
     <>
         <Header /> 
-        <Outlet />
+          <Outlet />
         <Footer />
     </>
 )
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <section className='error-page'>
+      <div className='center'>
+        <h2>Page Not Found</h2>
+        <Link to="/" className='btn primary'>Go Back Home</Link>
+      </div>
+    </section>
   )
 }
 
