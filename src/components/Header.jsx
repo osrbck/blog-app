4e3ccf97bde7720cbe6f93c5b5b78105559b98ff
Osman <osrbck@gmail.com>
2024-08-21 13:50:16 +0300

    src created

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../images/logo-meu.svg'
+import { FaBars } from 'react-icons/fa'
+import { AiOutlineClose } from 'react-icons/ai'
+
+const Header = () => {
+  return (
+
+    <nav>
+      <div className="container nav__container">
+        <Link to="/" className='nav__logo'>
+          <img src={Logo} alt="MEU logo" />
+        </Link>
+        <ul className="nav__menu">
+          <li><Link to="/profile">MND</Link></li>
+          <li><Link to="/create">Create Post</Link></li>
+          <li><Link to="/authors">Authors</Link></li>
+          <li><Link to="/logout">Logout</Link></li>
+        </ul>
+        <button className="nav__toggle-btn">
+          <AiOutlineClose/>
+        </button>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
