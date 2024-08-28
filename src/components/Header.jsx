fc2edbcc0c3f4284394904c2122d03a646a173c5
Osman <osrbck@gmail.com>
2024-08-28 13:48:51 +0300

    responsive design fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../images/logo-meu.svg'
 import { FaBars } from 'react-icons/fa'
 import { AiOutlineClose } from 'react-icons/ai'
 
 const Header = () => {
+  const [isNavShowing, setIsNavShowing] = useState(window.innerWidth > 800 ? true : false)
+
+  const closeNavHandler = () => {
+    if(window.innerWidth < 800)
+      setIsNavShowing(false)
+    else setIsNavShowing(true)
+  }
+
   return (
 
     <nav>
       <div className="container nav__container">
-        <Link to="/" className='nav__logo'>
+        <Link to="/" className='nav__logo' onClick={closeNavHandler}>
           <img src={Logo} alt="MEU logo" />
         </Link>
-        <ul className="nav__menu">
-          <li><Link to="/profile/osrbck">osrbck</Link></li>
-          <li><Link to="/create">Create Post</Link></li>
-          <li><Link to="/authors">Authors</Link></li>
-          <li><Link to="/logout">Logout</Link></li>
-        </ul>
-        <button className="nav__toggle-btn">
-          <AiOutlineClose/>
+        {isNavShowing && 
+          <ul className="nav__menu">
+            <li><Link to="/profile/osrbck" onClick={closeNavHandler}>osrbck</Link></li>
+            <li><Link to="/create" onClick={closeNavHandler}>Create Post</Link></li>
+            <li><Link to="/authors" onClick={closeNavHandler}>Authors</Link></li>
+            <li><Link to="/logout" onClick={closeNavHandler}>Logout</Link></li>
+          </ul>
+        }
+        <button className="nav__toggle-btn" onClick={() => setIsNavShowing(!isNavShowing)}>
+          {isNavShowing ? <AiOutlineClose/> : <FaBars/>}
+          
         </button>
       </div>
     </nav>
