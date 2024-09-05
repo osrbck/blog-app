aa7d18c7b88837e7005451f17bf9d4a44971bd0a
Osman <osrbck@gmail.com>
2024-09-05 16:55:02 +0300

    get all users, users by id and avatar-change

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const fs = require('fs')
+const path = require('path')
+const {v4: uuid} = require('uuid')
 
 const User = require('../models/userModel')
 const HttpError = require("../models/errorModel")
@@ -80,7 +83,16 @@ const loginUser = async (req, res, next) => {
 // POST : api/users/users/:id
 // PROTECTED
 const getUser = async (req, res, next) => {
-    res.json("User Profile")
+    try {
+        const {id} = req.params;
+        const user = await User.findById(id).select('-password');
+        if (!user) {
+            return next(new HttpError("User not found.", 404))
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 
@@ -88,7 +100,46 @@ const getUser = async (req, res, next) => {
 // POST : api/users/change-avatar
 // PROTECTED
 const changeAvatar = async (req, res, next) => {
-    res.json("Change User Avatar")
+    try {
+        if(!req.files.avatar)
+            return next(new HttpError("Please choose an image.", 422))
+        
+        // find user
+        const user = await User.findById(req.user.id)
+
+        //delete old avatar
+        if(user.avatar){
+            fs.unlink(path.join(__dirname, '..', 'uploads', user.avatar), (err) =>{
+                if(err) 
+                    return next(new HttpError(err));
+            })
+        }
+
+        const {avatar} = req.files;
+        //check file size
+        if(avatar.size > 500000)
+            return next(new HttpError("Profile picture is too big. Should be less than 500kb", 422));
+
+        let fileName;
+        fileName = avatar.name;
+        let splittedFileName = fileName.split('.');
+        let newFileName = splittedFileName[0] + uuid() + '.' + splittedFileName[splittedFileName.length -1]
+        avatar.mv(path.join(__dirname, '..', 'uploads', newFileName), async (err) =>{
+            if (err) {
+                return next(new HttpError(err));
+            }
+
+            const updatedAvatar = await User.findByIdAndUpdate(req.user.id, {avatar: fileName}, {new: true})
+
+            if(!updatedAvatar)
+                return next(new HttpError("Avatar could not be changed", 422));
+            res.status(200).json(updatedAvatar)
+        })
+
+        
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 
@@ -106,7 +157,12 @@ const editUser = async (req, res, next) => {
 // POST : api/users/authors
 // UNPROTECTED
 const getAuthors = async (req, res, next) => {
-    res.json("Get All users/authors")
+    try {
+        const authors = await User.find().select('-password');
+        res.json(authors);
+    } catch (error) {
+        return next(new HttpError(error)) 
+    }
 }
 
 
