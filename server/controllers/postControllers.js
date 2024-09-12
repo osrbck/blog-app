5b8325664c9c5febfc2d97891fd2954ac3224123
Osman <osrbck@gmail.com>
2024-09-12 16:27:05 +0300

    getPosts and post createPost

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -1,24 +1,61 @@
+const Post = require('../models/postModel')
+const User = require('../models/userModel')
 const fs = require('fs')
 const path = require('path')
 const {v4: uuid} = require('uuid')
-
 const HttpError = require("../models/errorModel")
 
-const Post = require('../models/postModel')
-const User = require('../models/userModel')
 
 // ========================== CREATE A POST
 // POST : api/posts
 // PROTECTED
 const createPost = async (req, res, next) => {
-    res.json("Create post")
+    try {
+        let {title, category, description} = req.body;
+        if(!title || !category || !description || !req.files){
+            return next(new HttpError("Fill in all fields and choose thumbnail.", 422));
+        }
+
+        const {thumbnail} = req.files;
+        if(thumbnail.size > 2000000){
+            return next(new HttpError("Thumbnail too big. File should be less than 2mb.", 422));
+        }
+
+        let fileName;
+        fileName = thumbnail.name;
+        let splittedFileName = fileName.split('.')
+        let newFileName = splittedFileName[0] + uuid() + "." + splittedFileName[splittedFileName.length -1]
+        thumbnail.mv(path.join(__dirname, '..', '/uploads', newFileName), async (err) => {
+            if(err){
+                return next(new HttpError(error));
+            } else{
+                const newPost = await Post.create({title, category, description, thumbnail : newFileName, creator: req.user.id})
+                if(!newPost){
+                    return next(new HttpError("Post could not be created.", 422));
+                }
+
+                const currentUser = await User.findById(req.user.id);
+                const userPostCount = currentUser.posts + 1;
+                await User.findByIdAndUpdate(req.user.id, {posts: userPostCount});
+
+                res.status(201).json(newPost);
+            }
+        })
+    } catch (error) {
+        return next(new HttpError(error));
+    }
 }
 
 // ========================== GET All POST
 // GET : api/posts
 // PROTECTED
 const getPosts = async (req, res, next) => {
-    res.json("Get all posts")
+    try {
+        const posts = await Post.find().sort({updatedAt: -1})
+        res.status(200).json(posts)
+    } catch (error) {
+        return next(new HttpError(error));
+    }
 }
 
 // ========================== GET SINGLE POST
--- server/models/postModel.js
+++ server/models/postModel.js
@@ -2,11 +2,10 @@ const { Schema, model } = require("mongoose")
 
 const postSchema = new Schema({
     title : {type: String, required: true},
-    category : {type: String, enum: ["Agriculture", "Business", "Education", "Entertainment", "Art", "Investment", "Uncategorized", "Weather"], message: "VALUE is not supported}"},
+    category : {type: String, enum: ["Agriculture", "Business", "Education", "Entertainment", "Art", "Investment", "Uncategorized", "Weather"], message: "VALUE is not supported"},
     description : {type: String, required: true},
-    thumbnail : {type: String, required: true},
-    creator : {type: Schema.Types.ObjectId, ref: "User"}
-
+    creator : {type: Schema.Types.ObjectId, ref: "User"},
+    thumbnail : {type: String, required: true}
 }, {timestamps: true})
 
 module.exports = model("Post", postSchema)
